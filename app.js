36cbae2f5c05f6813697b4bcaa28d47a50287c0d
Koné Trésor <kone.tresor123@gmail.com>
2022-12-10 08:19:11 +0100

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+//import
+const path = require("path");
+//import
+
+//third-party package import
+const express = require('express');
+//third-party package import
+
+const app = express();
+
+//templating engine setup
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//this will allow me to serve static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(5000)
