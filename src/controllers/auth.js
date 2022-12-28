8dce03e3dac8a2707a97159b6ce35b2db6172025
Koné Trésor <103995919+TresorKone@users.noreply.github.com>
2022-12-28 17:50:54 +0100

    signup WIP3

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-const { bcrypt } = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models').User;
 
@@ -16,6 +16,26 @@ exports.postSign = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    User.findOne({where: { email: email } })
+        .then(userCheck => {
+        if (userCheck) {
+            return console.log('already exist');
+        }else {
+            const user = new User({
+                email: email,
+                password: password
+            });
+            return user.save()
+                .then(r => {
+                    console.log('user created');
+                    res.redirect('/login')
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    })
+    /*
     const user = new User({
         email: email,
         password: password
@@ -28,4 +48,6 @@ exports.postSign = (req, res, next) => {
         .catch(err => {
             console.log(err);
         })
+
+     */
 };
